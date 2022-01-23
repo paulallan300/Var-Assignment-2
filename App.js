b57923142d2e05aeafb5aee3cb845f96eda2b953
Paul Allan <65020951+paulallan300@users.noreply.github.com>
2022-01-23 19:59:35 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './Button';
+import Sample from './var-assign-1/Sample';
+
+function App() {
+  return (
+    <>
+    <h2><Sample a="7" b="3" c="6" /></h2>
+    
+    </>
+  
+  );
+}
+
+export default App;
